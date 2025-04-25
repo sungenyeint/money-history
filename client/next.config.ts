0959f25afcde463835ed2f7dfbc66d321d78c0a8
sungenyeint <sungenyeint@gmail.com>
2025-04-25 20:57:17 +0630

    Revert "add next-pwa"
    This reverts commit 0b8aab65661b9c79c28476be5362428c4ec4ebce.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,11 +1,8 @@
-const withPWA = require("next-pwa")({
-  dest: "public", // Output directory for service worker and related files
-  register: true, // Automatically register the service worker
-  skipWaiting: true, // Activate the service worker immediately after installation
-});
+import type { NextConfig } from "next";
 
-module.exports = withPWA({
+const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.5', 'your-production-url.com'], // Replace with your actual IP + port
   reactStrictMode: true,
-  // Other Next.js configurations
-});
+};
+
+export default nextConfig;
